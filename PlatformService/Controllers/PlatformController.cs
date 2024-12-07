55a4dbbdd204cabf1fdec98fb7d1d5da3ce9e8d0
Tran Hoang Giang <Hgiangtb2793@gmail.com>
2024-12-07 22:54:34 +0700

    Add project files.

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDTO>> GetPlatforms()
+        {
+            var platforms = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDTO>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDTO> GetPlatformById(int id)
+        {
+            var platform = _repository.GetPlatformById(id);
+
+            if (platform == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<PlatformReadDTO>(platform));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDTO> CreatePlatform(PlatformCreateDTO dto)
+        {
+            var newPlatform = _mapper.Map<Platform>(dto);
+            _repository.CreatePlatform(newPlatform);
+            _repository.SaveChanges();
+
+            var returnedPlatform = _mapper.Map<PlatformReadDTO>(newPlatform);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {id = returnedPlatform.Id}, returnedPlatform);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDB.cs
@@ -0,0 +1,31 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDB
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<ApplicationDbContext>());
+            }
+        }
+
+        public static void SeedData(ApplicationDbContext context)
+        {
+            if (context.Platforms.Any())
+            {
+                return;
+            }
+
+            context.Platforms.AddRange(
+                new Platform() { Name = ".NET", Publisher = "Microsoft", Cost = "Free"},
+                new Platform() { Name = "Sql Server Express", Publisher = "Microsoft", Cost = "Free"},
+                new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"}
+                );
+
+            context.SaveChanges();
+        }
+    }
+}
