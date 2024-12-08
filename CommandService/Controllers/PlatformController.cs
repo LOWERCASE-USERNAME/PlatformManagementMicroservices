e103b0d4033347e67ea7336d5ca2aaab9456199c
Tran Hoang Giang <Hgiangtb2793@gmail.com>
2024-12-08 20:52:20 +0700

    Init Command Service

---
+++ CommandService/Controllers/PlatformController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/command/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+    }
+}
